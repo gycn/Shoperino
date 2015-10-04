05c6e556e8647185939e917a044ee21f40544942
gycn <gary88111@gmail.com>
2015-10-03 20:28:52 -0700

    first commit

---
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.parse.starter;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.parse.LogInCallback;
+import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+
+public class MainActivity extends ActionBarActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    ParseAnalytics.trackAppOpenedInBackground(getIntent());
+    ParseUser curr = ParseUser.getCurrentUser();
+    if (curr == null) {
+      final Button button = (Button) findViewById(R.id.login_button);
+      button.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+          ParseFacebookUtils.logInWithReadPermissionsInBackground(this, permissions, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException err) {
+              if (user == null) {
+                Log.d("MyApp", "Uh oh. The user cancelled the Facebook login.");
+              } else if (user.isNew()) {
+                Log.d("MyApp", "User signed up and logged in through Facebook!");
+              } else {
+                Log.d("MyApp", "User logged in through Facebook!");
+              }
+            }
+          });
+        }
+      });
+    }
+    else
+    {
+
+    }
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+}
